6bb726607dba847a974aba4c53655e48cc60ec21
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-08-17 13:52:48 +0200

    WebSecurityConfig

--- src/main/java/pl/bykowski/currencygame/config/WebSecurityConfig.java
+++ src/main/java/pl/bykowski/currencygame/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.bykowski.currencygame.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -18,15 +20,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Jan")
-                .password(getPasswordEncoder().encode("Jan123"))
-                .roles("ADMIN");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/game").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
     }
 }
