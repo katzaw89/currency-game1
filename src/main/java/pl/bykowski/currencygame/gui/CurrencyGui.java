a353dd5f038856a1db3bd1d30b4001ef9c6dd061
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-08-17 12:16:57 +0200

    win

--- src/main/java/pl/bykowski/currencygame/gui/CurrencyGui.java
+++ src/main/java/pl/bykowski/currencygame/gui/CurrencyGui.java
@@ -1,6 +1,7 @@
 package pl.bykowski.currencygame.gui;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,21 +12,25 @@ import pl.bykowski.currencygame.controller.CurrencyClient;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Route("game")
 public class CurrencyGui extends VerticalLayout {
 
     @Autowired
     public CurrencyGui(CurrencyClient currencyClient) {
+        AtomicInteger counter = new AtomicInteger();
+        Label labelFinnalResult = new Label("Liczba podejść");
+
         Double eur = currencyClient.getCurrencyRates().getRates().getEUR();
         BigDecimal bigDecimalRawCurrency = new BigDecimal(eur);
-        BigDecimal bigDecimalPlnValue  = new BigDecimal(1);
+        BigDecimal bigDecimalPlnValue = new BigDecimal(1);
         BigDecimal bigDecimalToGuess = bigDecimalPlnValue.divide(bigDecimalRawCurrency, 2, RoundingMode.CEILING);
 
         Label labelCurrencyFrom = new Label("Z waluty");
-        labelCurrencyFrom.setText("PLN");
+        labelCurrencyFrom.setText("Z EUR");
         Label labelCurrencyTo = new Label("Na walute");
-        labelCurrencyTo.setText("EUR");
+        labelCurrencyTo.setText("NA PLN");
         HorizontalLayout hl1 = new HorizontalLayout();
         hl1.add(labelCurrencyFrom, labelCurrencyTo);
 
@@ -40,17 +45,22 @@ public class CurrencyGui extends VerticalLayout {
             String greaterMessage = "za mało";
             if (result == 0) {
                 labelResult.setText(winMessage);
+                labelFinnalResult.setText("GRATULACJE! udao się za " + counter.get());
+
+                add(new Image("https://media1.giphy.com/media/2sXf9PbHcEdE1x059I/giphy.gif","super!"));
+
             } else if (result >= 1) {
                 labelResult.setText(smallMessage);
+                labelFinnalResult.setText("Liczba podejść: " + counter.incrementAndGet());
             } else {
                 labelResult.setText(greaterMessage);
+                labelFinnalResult.setText("Liczba podejść: " + counter.incrementAndGet());
             }
         });
 
         HorizontalLayout hl2 = new HorizontalLayout();
         hl2.add(textFieldUserValue, button, labelResult);
 
-        Label labelFinnalResult = new Label("Liczba podejść");
 
         add(hl1, hl2, labelFinnalResult);
     }
